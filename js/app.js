d73161e218fd4516a892dc3c77b506845c01c2e6
João Janot <janot1404@gmail.com>
2024-05-15 11:11:51 -0300

    Comentando funções

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
-
-function atualizarIngressosDisponiveis(tipoIngresso, quantidade) {
-    let ingressosDisponiveis = parseInt(document.getElementById(`qtd-${tipoIngresso}`).textContent);
-    if (ingressosDisponiveis >= quantidade) {
-        let ingressosRestantes = ingressosDisponiveis - quantidade;
-        document.getElementById(`qtd-${tipoIngresso}`).innerHTML = `${ingressosRestantes}`;
-    } else {
-        alert(`Não há ${quantidade} ingressos disponíveis para o setor de ${tipoIngresso}.`);
-    }
-}
-
+//function chamada no botão
 function comprar() {
     //Coletando dados
     let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
-
     //Adquirindo tipo de ingresso
     if (tipoDeIngresso == 'inferior') {
         atualizarIngressosDisponiveis('inferior', quantidade);
@@ -21,5 +10,15 @@ function comprar() {
         atualizarIngressosDisponiveis('superior', quantidade);
     } else if (tipoDeIngresso == 'pista') {
         atualizarIngressosDisponiveis('pista', quantidade);
+    }
+}
+//function para reduzir o código
+function atualizarIngressosDisponiveis(tipoIngresso, quantidade) {
+    let ingressosDisponiveis = parseInt(document.getElementById(`qtd-${tipoIngresso}`).textContent);
+    if (ingressosDisponiveis >= quantidade) {
+        let ingressosRestantes = ingressosDisponiveis - quantidade;
+        document.getElementById(`qtd-${tipoIngresso}`).innerHTML = `${ingressosRestantes}`;
+    } else {
+        alert(`Não há ${quantidade} ingressos disponíveis para o setor de ${tipoIngresso}.`);
     }
 }
