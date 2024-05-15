0d1a84f975e12d75d56cfb36c8de4099f7d7b1fe
João Janot <janot1404@gmail.com>
2024-05-15 11:10:02 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+function atualizarIngressosDisponiveis(tipoIngresso, quantidade) {
+    let ingressosDisponiveis = parseInt(document.getElementById(`qtd-${tipoIngresso}`).textContent);
+    if (ingressosDisponiveis >= quantidade) {
+        let ingressosRestantes = ingressosDisponiveis - quantidade;
+        document.getElementById(`qtd-${tipoIngresso}`).innerHTML = `${ingressosRestantes}`;
+    } else {
+        alert(`Não há ${quantidade} ingressos disponíveis para o setor de ${tipoIngresso}.`);
+    }
+}
+
+function comprar() {
+    //Coletando dados
+    let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    //Adquirindo tipo de ingresso
+    if (tipoDeIngresso == 'inferior') {
+        atualizarIngressosDisponiveis('inferior', quantidade);
+    } else if (tipoDeIngresso == 'superior') {
+        atualizarIngressosDisponiveis('superior', quantidade);
+    } else if (tipoDeIngresso == 'pista') {
+        atualizarIngressosDisponiveis('pista', quantidade);
+    }
+}
